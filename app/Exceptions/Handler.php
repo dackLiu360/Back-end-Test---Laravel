d07e4af8c5481d2f522796011687a887435ccf8f
Lucas Domingues dos Santos <lucas.santos@eduxe.com.br>
2021-08-30 10:38:29 -0300

    added exception handler to 404 requests

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -36,6 +37,14 @@ class Handler extends ExceptionHandler
     {
         $this->reportable(function (Throwable $e) {
             //
+        });
+
+        $this->renderable(function (NotFoundHttpException $exception, $request) {
+            if ($request->is('api/*')) {
+                return response()->json(
+                    'Not Found method!'
+                , 404);
+            }
         });
     }
 }
